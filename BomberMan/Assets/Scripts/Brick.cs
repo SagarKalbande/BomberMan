2e99f910433b9f0daccccc97081bb4f7036b7572
unknown <sagarkalbande1993@gmail.com>
2019-06-01 02:10:24 +0530

    files remaining

---
+++ BomberMan/Assets/Scripts/Brick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    public GameObject BrickRef;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i, j;
+        for (i = -4; i <= 4; i++)
+        {
+            for (j = -4; j <= 4; j++)
+            {
+                if ((i % 2 == 1 || i % 2 == -1 || !((i + j) % 2 == 0)))
+                {
+                    if (!(i == 0 && j == 0))
+                    {
+                        Instantiate(BrickRef, new Vector3(i * 2, j * 2, 0), Quaternion.identity);
+                    }
+                }
+
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
