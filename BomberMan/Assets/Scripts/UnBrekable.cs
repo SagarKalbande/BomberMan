43eba71e0b3afdcc128a5c83beb6b2bf72b37344
sagarkalbande <sagarkalbande1993@gmail.com>
2019-06-01 01:59:09 +0530

    Outer Grid commit

--- BomberMan/Assets/Scripts/UnBrekable.cs
+++ BomberMan/Assets/Scripts/UnBrekable.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class UnBrekable : MonoBehaviour
 {
+    public GameObject unbreakableObject;
+    public int z;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var InstantiateHrz= Instantiate(unbreakableObject, new Vector3(4*z+2, 4*z+2, 0), Quaternion.Euler(0, 0, 90));
+        for (int i = -z; i <=z ; i++)
+        {
+            for (int j = -z; j <= z; j++)
+            {
+                Instantiate(unbreakableObject, new Vector3(i * 4.0F, j * 4.0F, 0), Quaternion.identity);
+            }
+        }
+        InstantiateHrz.transform.localScale = new Vector3(100, 1, 1);
+        Instantiate(InstantiateHrz, new Vector3(0, -4*z-2, 0), Quaternion.identity);
+        Instantiate(InstantiateHrz, new Vector3(-4*z-2, 4*z+2, 0), Quaternion.Euler(0, 0, 90));
+        Instantiate(InstantiateHrz, new Vector3(0, 4*z+2, 0), Quaternion.identity);
+
     }
 
     // Update is called once per frame
